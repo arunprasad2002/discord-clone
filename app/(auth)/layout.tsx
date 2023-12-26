435ee72e65ba856da382a80d1946bc5826457cd4
arun mahato <ptech784@gmail.com>
2023-12-26 11:37:17 +0530

    add - auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            {children}
+        </div>
+
+    )
+}
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <UserButton afterSignOutUrl='/' />
       <Button variant={'default'}>Get Started</Button>
     </div>
   )
